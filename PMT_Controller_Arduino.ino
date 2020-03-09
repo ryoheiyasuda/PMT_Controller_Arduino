9ff1d2182b15946bce48d4ffba51d8b1c1aa1d7d
nzmysd <nozomi.yasuda.4649@gmail.com>
2020-03-09 10:52:42 -0400

    adjusted for 2 channels

--- PMT_Controller_Arduino.ino
+++ PMT_Controller_Arduino.ino
@@ -1,6 +1,10 @@
 /*
     COPYRIGHT nozomi yasuda
 
+    TODO: 
+    ====
+    Pin numbers are random, need to be adjusted 
+
     Serial input commands:
     ======================
     v_i%d = set voltage to %d (like v_i50)
@@ -11,99 +15,78 @@
     powA0 = turn off PMT A.
     autA1 = auto-recovery upon PMT A error. (default off)
     autA0 = turn off above feature.
+
 */
 
 #include <EEPROM.h>
 
-/*
-  #define outputA 50 //rotary encoder 1st pin
-  #define outputB 52 //rotary encoder 2nd pin
-  #define pelPowPin 4 //External connector pin number: 3
-  #define pelErrPin 5 //External connector pin number: 5
-  #define pmtPowPin 6 //External connector pin number: 7
-  #define pmtErrPin 7 //External connector pin number: 9
-  #define pmtOnPin 8 //External connector pin number: 11
-  #define vltOutputPin 10 //External connector pin number: 17
-  #define vMonitorInputPin A0 //External connector pin number: 15
-  #define fanPowPin 12 //External connector pin number: 19
+const short nPMT = 2; //number of PMT's connected to arduino 
 
-  #define vMonitorOutputPin 9 //Output for vMonitorInput.
-  #define ledPin 3 //Signal for PMT on/off and flickers during error
-  #define ledGNDPin 2
-*/
+//Pins 
+const short pelPowPin[nPMT] = {3,5}; //External connector pin number: 3 
+const short pelErrPin[nPMT] = {6,7}; //External connector pin number: 5 
+const short pmtPowPin[nPMT] = {8,9}; //External connector pin number: 7  
+const short pmtErrPin[nPMT] = {10,11}; //External connector pin number: 9 
+const short pmtOnPin[nPMT] = {12,13}; //External connector pin number: 11
+const short vltOutputPin[nPMT] = {14,15}; //External connector pin number: 17 
+const short vMonitorInputPin[nPMT] = {A0, A1}; //External connector pin number: 15 
+const short fanPowPin[nPMT] = {18,19}; //External connector pin number: 19
+const short vMonitorOutputPin[nPMT] = {20,21}; //Output for vMonitorInput.
+const short ledPin[nPMT] = {22,23}; //Signal for PMT on/off and flickers during error 
+const short ledGNDPin[nPMT] = {24,25};   
 
-#define pelPowPin 4 //External connector pin number: 3
-#define pelErrPin 5 //External connector pin number: 5
-#define pmtPowPin 6 //External connector pin number: 7
-#define pmtErrPin 7 //External connector pin number: 9
-#define pmtOnPin 8 //External connector pin number: 11
-#define vltOutputPin 10 //External connector pin number: 17
-#define vMonitorInputPin A0 //External connector pin number: 15
-#define fanPowPin 12 //External connector pin number: 19
-
-#define vMonitorOutputPin 9 //Output for vMonitorInput.
-#define ledPin 3 //Signal for PMT on/off and flickers during error 
-#define ledGNDPin 2
-
-const short rotaryPinA[2] = {50, 51};  //rotary encoder 1st pin
-const short rotaryPinB[2] = {52, 53};
-const short nPMT = 2;
-const short counterAddress[2] = {2, 4}; //stores counter in EEPROM
+//Rotary encoder and counter
+short counter[nPMT] = {0, 0};
+short rotaryCurrentState[nPMT] = {0, 0};
+short rotaryLastState[nPMT] = {0, 0};
+const short rotaryPinA[nPMT] = {50, 51};  
+const short rotaryPinB[nPMT] = {52, 53};
+const short counterAddress[nPMT] = {2, 4}; //stores counter in EEPROM
 const short counterMax = 50; //maximum voltage 0.9V is roughly 50 in 8-bit
 
-
-//Rotary encoder
-short counter[2] = {0, 0};
-short aState[2] = {0, 0};
-short aLastState[2] = {0, 0};
-
 //PMT states
-bool pmt_err = false;
-bool pel_err = false;
-bool pmt_on = false;
-bool vlt_change[2] = {true, true};
-bool auto_pmt_recov = false; //Auto-recovery on off.
+bool pmt_err[nPMT] = {false, false};
+bool pel_err[nPMT] = {false, false};
+bool pmt_on[nPMT] = {false, false};
+bool vlt_change[nPMT] = {true, true};
+bool auto_pmt_recov[nPMT] = {false, false}; //Auto-recovery on off.
 
-double v_monitor = 0;
-double v_input_calib = 1024.0; //10 bit input. Pretty noisy.
-double v_output_calib = 256.0; //8 bit output
-double vdd = 5.0;
-int ledBrightness = 20; //of 255
+//adjustments
+double v_monitor[nPMT] = {0, 0};
+double v_input_calib[nPMT] = {1024.0, 1024.0}; //10 bit input. Pretty noisy.
+double v_output_calib[nPMT] = {256.0, 256.0}; //8 bit output
+double vdd[nPMT] = {5.0, 5.0};
+int ledBrightness = 20; //of 255 
 
 //For averaging voltage input.
-double sum_vol = 0;
-int sum_count = 0;
+double sum_vol[nPMT] = {0, 0};
+int sum_count[nPMT] = {0, 0};
 int iter = 50; //number of iteration for average voltage for arduino
-
 
 void setup() {
   for (short i = 0; i < nPMT; i++) {
     pinMode (rotaryPinA[i], INPUT);
     pinMode (rotaryPinB[i], INPUT);
-  }
-  pinMode (pelPowPin, OUTPUT);
-  pinMode (pelErrPin, INPUT);
-  pinMode (pmtPowPin, OUTPUT);
-  pinMode (pmtErrPin, INPUT);
-  pinMode (pmtOnPin, INPUT);
-  pinMode (vltOutputPin, OUTPUT); //analog
-  pinMode (vMonitorOutputPin, OUTPUT); //analog
-  pinMode (vMonitorInputPin, INPUT);
-  pinMode (fanPowPin, OUTPUT);
-  pinMode (ledPin, OUTPUT);
-  pinMode (ledGNDPin, OUTPUT);
-
-
-  digitalWrite (pmtPowPin, LOW);
-  digitalWrite (pelPowPin, LOW);
-  digitalWrite (fanPowPin, LOW);
-  digitalWrite (ledGNDPin, LOW);
-
+    pinMode (pelPowPin[i], OUTPUT);
+    pinMode (pelErrPin[i], INPUT);
+    pinMode (pmtPowPin[i], OUTPUT);
+    pinMode (pmtErrPin[i], INPUT);
+    pinMode (pmtOnPin[i], INPUT);
+    pinMode (vltOutputPin[i], OUTPUT); //analog
+    pinMode (vMonitorOutputPin[i], OUTPUT); //analog
+    pinMode (vMonitorInputPin[i], INPUT);
+    pinMode (fanPowPin[i], OUTPUT);
+    pinMode (ledPin[i], OUTPUT);
+    pinMode (ledGNDPin[i], OUTPUT);
+    digitalWrite (pmtPowPin[i], LOW);
+    digitalWrite (pelPowPin[i], LOW);
+    digitalWrite (fanPowPin[i], LOW);
+    digitalWrite (ledGNDPin[i], LOW);
+   }
   Serial.begin (9600);
-
   for (short i = 0; i < nPMT; i++)
   {
-    aLastState[i] = digitalRead(rotaryPinA[i]);
+    rotaryLastState[i] = digitalRead(rotaryPinA[i]);
     EEPROM.get(counterAddress[i], counter[i]);
     vltChange(i);
   }
@@ -113,58 +96,62 @@ void rotaryEncoderProcess()
 {
   for (short i = 0; i < nPMT; i++)
   {
-    aState[i] = digitalRead(rotaryPinA[i]);
-    if (aState[i] != aLastState[i]) {
-      if (digitalRead(rotaryPinB[i]) != aState[i]) {
+    rotaryCurrentState[i] = digitalRead(rotaryPinA[i]);
+    if (rotaryCurrentState[i] != rotaryLastState[i]) {
+      if (digitalRead(rotaryPinB[i]) != rotaryCurrentState[i]) {
         counter[i] ++;
       } else {
         counter[i] --;
       }
       vlt_change[i] = true;
     }
-    aLastState[i] = aState[i];
+    rotaryLastState[i] = rotaryCurrentState[i];
   }
 }
 
 void pmtRead()
 {
-  pel_err = digitalRead(pelErrPin);
-  pmt_err = digitalRead(pmtErrPin);
-  pmt_on = digitalRead(pmtOnPin);
-
-  if (pmt_err) {
-    //Serial.println("pmt error");
-    if (auto_pmt_recov)
-    {
-      digitalWrite (pmtPowPin, LOW);
-      delay(100);
-      digitalWrite (pmtPowPin, HIGH);
-    }
-    else
-    {
-      analogWrite (ledPin, ledBrightness);
-      delay(400);
-      digitalWrite (ledPin, 0);
-      delay(400);
-    }
-  }
-  else if (pmt_on) {
-    analogWrite (ledPin, ledBrightness);
-  }
-  else {
-    analogWrite (ledPin, 0);
-  }
-
-  sum_count ++;
-  sum_vol += (double)analogRead(vMonitorInputPin);
-  if (sum_count == iter)
+  for (short i = 0; i < nPMT; i++)
   {
-    v_monitor = sum_vol * vdd / v_input_calib / iter;
-    sum_vol = 0;
-    sum_count = 0;
-    analogWrite (vMonitorOutputPin, v_monitor * v_output_calib / vdd);
+    pel_err[i] = digitalRead(pelErrPin[i]);
+    pmt_err[i] = digitalRead(pmtErrPin[i]);
+    pmt_on[i] = digitalRead(pmtOnPin[i]);
+  
+    if (pmt_err[i]) {
+      //Serial.println("pmt error");
+      if (auto_pmt_recov[i])
+      {
+        digitalWrite (pmtPowPin[i], LOW);
+        delay(100);
+        digitalWrite (pmtPowPin[i], HIGH);
+      }
+      else
+      {
+        analogWrite (ledPin[i], ledBrightness);
+        delay(400);
+        digitalWrite (ledPin[i], 0);
+        delay(400);
+      }
+    }
+    else if (pmt_on[i]) {
+      analogWrite (ledPin[i], ledBrightness);
+    }
+    else {
+      analogWrite (ledPin[i], 0);
+    }
+
+
+  sum_count[i] ++;
+  sum_vol[i] += (double)analogRead(vMonitorInputPin[i]);
+  if (sum_count[i] == iter)
+  {
+    v_monitor[i] = sum_vol[i] * vdd[i] / v_input_calib[i] / iter;
+    sum_vol[i] = 0;
+    sum_count[i] = 0;
+    analogWrite (vMonitorOutputPin[i], v_monitor[i] * v_output_calib[i] / vdd[i]);
   }
   delay(1);
+  }
 }
 
 void pmtWrite()
@@ -181,7 +168,7 @@ void pmtWrite()
     {
       if (s.length() == 4)
       {
-        Serial.println((double)counter[pmtID] / v_output_calib * vdd);
+        Serial.println((double)counter[pmtID] / v_output_calib[pmtID] * vdd[pmtID]);
       }
       else
       {
@@ -192,40 +179,40 @@ void pmtWrite()
     }
     else if (s3.equals("v_m"))//monitor
     {
-      Serial.println(v_monitor);
+      Serial.println(v_monitor[pmtID]);
 
     }
     else if (s3.equals("pmt")) {
       int val = s.substring(4).toInt();
       if (val == 1) {
-        pmtOnOff(true);
+        pmtOnOff(true, pmtID);
       }
       else {
-        pmtOnOff(false);
+        pmtOnOff(false, pmtID);
       }
     }
     else if (s3.equals("pow")) {
       bool val = (bool)(s.substring(4).toInt());
-      powerOnOff(val);
+      powerOnOff(val, pmtID);
     }
     else if (s3.equals("aut")) {
       bool val = (bool)(s.substring(4).toInt());
-      auto_pmt_recov = val;
+      auto_pmt_recov[pmtID] = val;
     }
   }
 }
 
-void pmtOnOff(bool ON)
+void pmtOnOff(bool ON, short pmtID)
 {
-  digitalWrite (pmtPowPin, ON);
+  digitalWrite (pmtPowPin[pmtID], ON);
 }
 
-void powerOnOff(bool ON)
+void powerOnOff(bool ON, short pmtID)
 {
   if (!ON) {
-    pmtOnOff(false);
+    pmtOnOff(false, pmtID);
   }
-  digitalWrite (pelPowPin, ON);
+  digitalWrite (pelPowPin[pmtID], ON);
 }
 
 void vltChange(short pmtID) {
